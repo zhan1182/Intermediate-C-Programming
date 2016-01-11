373481d2ec6a850eda8aac0afedac69cc9c4b318
Jinyi Zhang <zhan1182@purdue.edu>
2016-01-11 13:19:14 -0500

    add string op 2

---
+++ string_operation_2/answer02.c
@@ -0,0 +1,48 @@
+
+#include "answer02.h"
+
+size_t my_strlen(const char * str)
+{
+    return 0;
+}
+
+int my_countchar(const char * str, char c)
+{
+    return 0;
+}
+
+char * my_strchr(const char * str, int ch)
+{
+    return NULL;
+}
+
+char * my_strrchr(const char * str, int ch)
+{
+    return NULL;
+}
+
+char * my_strstr(const char * haystack, const char * needle)
+{
+    return NULL;
+}
+
+char * my_strcpy(char * dest, const char * src)
+{
+    return NULL;
+}
+
+char * my_strcat(char * dest, const char * src)
+{
+    return NULL;
+}
+
+int my_isspace(int ch)
+{
+    return 0;
+}
+
+int my_atoi(const char * str)
+{
+    return 0;
+}
+
